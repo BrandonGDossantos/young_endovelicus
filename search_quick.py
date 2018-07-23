05f89aea9ca62b206d1fa9f54742f96d1d845a6b
BrandonGDossantos <bxd7887@g.rit.edu>
2018-07-22 22:16:19 -0400

    Added something

--- search_quick.py
+++ search_quick.py
@@ -23,36 +23,15 @@ def ndbno_lookup(ndb, ndbno):
     res = NDB.ndbno_lookup(ndbno)
     print(json.dumps(res, sort_keys=True, indent=4))
 
-def compute_convert(name, eqv_in_g, value_in_g):
-    eqv_to_oz = eqv_in_g * 0.0035
-    normalized_oz = round((1/eqv_to_oz)/10, 2)
-    normalized_value = round(float(value_in_g) * float(normalized_oz), 2)
-    print("\t {} units per oz".format(normalized_value))
-    return normalized_value
+def calc_total(ingredient_map):
+    pass
+    total = {}
+    for key, value in ingredient_map.items():
 
-def converter(ndb, item):
-    nutrient_map = {}
-    res = NDB.list_standard(item)
-    for item in res['list']['item']:
-        print("[{}]: {}".format(item['offset'] + 1, item['name']))
-    choice = input("Pick the item: ")
-    res = NDB.ndbno_lookup(str(res['list']['item'][int(choice)-1]['ndbno']))
-    counter = 1
-    for measure in res['foods'][0]['food']['nutrients'][0]['measures']:
-        print("[{}]: {} {}".format(counter, measure['qty'], measure['label']))
-        counter += 1
-        print('\n')
-    choice = input("Choose your prefered measurement: ")
-    for measurement in res['foods'][0]['food']['nutrients']:
-        name = measurement['name']
-        print("{}".format(name))
-        nutrient_map[name] = compute_convert(name, measurement['measures'][int(choice)-1]['eqv'], measurement['measures'][int(choice)-1]['value'])
-    return nutrient_map
+       total = {k : v+total[k] for k, v in value.items() }
+    return total
 
-
-if __name__ == "__main__":
-    api_key = "6I7oF4v5gemv0hZvOZH7pzFGRQzwLKGS5A4y3vWQ" 
-    NDB = NDB(api_key)
+def ingredient_populator():
     ingredient_map = {}
     yes = {'yes','y', 'ye' , ''}
     no = {'no','n'}
@@ -67,9 +46,51 @@ if __name__ == "__main__":
             x = False
         else:
             sys.stdout.write("Please respond with 'yes' or 'no'")
-    print(json.dumps(ingredient_map, sort_keys=True, indent=4))
+    # ingredient_map['total'] = calc_total(ingredient_map)
+    return ingredient_map 
 
-    # list_standard(NDB, "sliced bread")
+def algorithm(ingredient_map):
+    for key, value in ingredient_map.items():
+        ingredient_map[key] = {k : v*2 for k, v in value.items() }
+    return ingredient_map
+
+
+def compute_convert(name, eqv_in_g, value_in_g):
+    eqv_to_oz = eqv_in_g * 0.0035
+    normalized_oz = round((1/eqv_to_oz)/10, 2)
+    normalized_value = round(float(value_in_g) * float(normalized_oz), 2)
+    return normalized_value
+
+def converter(ndb, item):
+    nutrient_map = {}
+    res = NDB.list_standard(item)
+    for item in res['list']['item']:
+        print("[{}]: {}".format(item['offset'] + 1, item['name']))
+    choice = input("Pick the item: ")
+    res = NDB.ndbno_lookup(str(res['list']['item'][int(choice)-1]['ndbno']))
+    counter = 1
+    for measure in res['foods'][0]['food']['nutrients'][0]['measures']:
+        print("[{}]: {} {}".format(counter, measure['qty'], measure['label']))
+        counter += 1
+    choice = input("Choose your prefered measurement: ")
+    for measurement in res['foods'][0]['food']['nutrients']:
+        name = measurement['name']
+        nutrient_map[name] = compute_convert(name, measurement['measures'][int(choice)-1]['eqv'], measurement['measures'][int(choice)-1]['value'])
+    nutrient_map['oz'] = 1
+    return nutrient_map
+
+
+if __name__ == "__main__":
+    api_key = "6I7oF4v5gemv0hZvOZH7pzFGRQzwLKGS5A4y3vWQ" 
+    NDB = NDB(api_key)
+    ingredient_map = ingredient_populator()
+
+    print(json.dumps(ingredient_map, sort_keys=True, indent=4))
+    # print(json.dumps(algorithm(ingredient_map), sort_keys=True, indent=4))
+    # algorithm(ingredient_map)
+        
+
+    # list_standard(NDB, "slice wheat bread")
     # list_branded(NDB, "715134255720")
     # ndbno_lookup(NDB, "09038")
 
